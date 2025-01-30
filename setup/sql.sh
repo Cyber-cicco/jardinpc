6fb33766f85cc322a5b7cac3ebc65a0c48e09ce5
Abel Ciccoli <abelcoli@outlook.fr>
2025-01-30 01:01:19 +0100

    oupsi(oupsi): oupsi

--- setup/sql.sh
+++ setup/sql.sh
@@ -1,4 +1,4 @@
-~/go/bin/jet -dsn='mariadb://jardin:pazo1928!@tcp(localhost:3306)/jardinpc' -path=./.gen
+~/go/bin/jet -dsn='mariadb://jardin:pazo1928!@tcp(localhost:3306)/jardinpc' -path=./.gen # Promis juré ce mdp n'est nul part en production
 rm -rf ../.gen
 mv .gen/ ../.gen
 
